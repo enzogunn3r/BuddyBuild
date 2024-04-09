14c6d5603f09514a3178ca56716e30d7b05e3fdd
ege gencer <egencer2626@gmail.com>
2024-04-09 22:39:56 +0200

    Adds icons and navigation

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,6 +4,10 @@ import Card from '../components/BuddyCard';
 import users from '../../assets/data/users';
 import AnimatedStack from '../components/AnimatedStack';
 
+import Entypo from 'react-native-vector-icons/Entypo';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
 
 const HomeScreen = () => {
 
@@ -23,6 +27,23 @@ const HomeScreen = () => {
           onSwipeLeft={onSwipeLeft}
           onSwipeRight={onSwipeRight}
         />
+        <View style= {styles.icons}>
+            <View style={styles.button}>
+                <FontAwesome name="undo" size={30} color="#FBD88B" />
+            </View>
+            <View style={styles.button}>
+                <Entypo name="cross" size={30} color="#F76C6B" />
+            </View>
+            <View style={styles.button}>
+                <FontAwesome name="star" size={30} color="#3AB4CC" />
+            </View>
+            <View style={styles.button}>
+                <FontAwesome name="heart" size={30} color="#4FCC94" />
+            </View>
+            <View style={styles.button}>
+                <Ionicons name="flash" size={30} color="#A65CD2" />
+            </View>
+        </View>
     </View>
   );
 };
@@ -33,6 +54,22 @@ const styles = StyleSheet.create({
     alignItems: 'center', 
     flex: 1,
     width: "100%",
+    backgroundColor: '#ededed'
+  },
+  icons: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+    padding:10,
+  },
+  button: {
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 50,
+    justifyContent: 'center',
+    allignItems: 'center',
+    width: 50,
+    height: 50,
   },
 })
 
